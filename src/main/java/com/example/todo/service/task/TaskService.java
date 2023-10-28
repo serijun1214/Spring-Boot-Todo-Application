5758e69cda23408ab5c494e132a3d0524d6195f3
Junki Seri <junki.seri@rakus-partners.co.jp>
2023-10-28 11:55:56 +0900

    TaskEntityの追加

--- src/main/java/com/example/todo/service/task/TaskService.java
+++ src/main/java/com/example/todo/service/task/TaskService.java
@@ -1,23 +1,21 @@
 package com.example.todo.service.task;
 
-import com.example.todo.controller.task.TaskDTO;
-
 import java.util.List;
 
 public class TaskService {
 
-    public List<TaskDTO> find() {
-        var task1 = new TaskDTO(
+    public List<TaskEntity> find() {
+        var task1 = new TaskEntity(
                 1L,
                 "Spring Bootを 学ぶ",
                 "TODO アプリケーションを作ってみる",
-                "TODO"
+                TaskStatus.TODO
         );
-        var task2 = new TaskDTO(
+        var task2 = new TaskEntity(
                 1L,
                 "Spring Securityを 学ぶ",
                 "ログイン機能を作ってみる",
-                "TODO"
+                TaskStatus.DOING
         );
         return List.of(task1, task2);
     }
