e55c18299789fc8097f4b2427219465a6e08cb5d
serijun1214 <junki.seri@rakus-partners.co.jp>
2023-10-31 01:38:48 +0900

    バリデーションチェックを追加

--- src/main/java/com/example/todo/controller/task/TaskController.java
+++ src/main/java/com/example/todo/controller/task/TaskController.java
@@ -4,6 +4,8 @@ import com.example.todo.service.task.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,7 +43,11 @@ public class TaskController {
     }
 
     @PostMapping
-    public String create(TaskForm form, Model model) {
+    public String create(@Validated TaskForm form, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "tasks/form";
+        }
+
         taskService.create(form.toEntity());
         return "redirect:/tasks";
     }
