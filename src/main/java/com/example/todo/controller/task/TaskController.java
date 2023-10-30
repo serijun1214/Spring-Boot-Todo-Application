ce99dfd367ab7aae6acf281faf9dec4707594bfd
serijun1214 <junki.seri@rakus-partners.co.jp>
2023-10-31 04:12:50 +0900

    存在しないタスクIDの編集ページが呼ばれたときは404エラーを表示

--- src/main/java/com/example/todo/controller/task/TaskController.java
+++ src/main/java/com/example/todo/controller/task/TaskController.java
@@ -29,7 +29,7 @@ public class TaskController {
     @GetMapping("/{id}")
     public String showDetail (@PathVariable("id") long taskId, Model model) {
         var taskEntity = taskService.findById(taskId)
-                .orElseThrow(() -> new IllegalArgumentException("Task not found: id" + taskId));
+                .orElseThrow(TaskNotFoundException::new);
         model.addAttribute("task", TaskDTO.toDTO(taskEntity));
         return "tasks/detail";
     }
@@ -52,7 +52,7 @@ public class TaskController {
     @GetMapping("/{id}/editForm")
     public String showEditForm(@PathVariable("id") long id, Model model) {
         var taskEntiry = taskService.findById(id)
-                        .orElseThrow(() -> new IllegalArgumentException("Task not found"));
+                        .orElseThrow(TaskNotFoundException::new);
         var form = new TaskForm(taskEntiry.summary(), taskEntiry.description(), taskEntiry.status().name());
         model.addAttribute("taskForm", form);
         return "tasks/form";
