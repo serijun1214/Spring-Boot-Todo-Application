013a29663dc862f6108daddc4641b3ba01fba74d
serijun1214 <junki.seri@rakus-partners.co.jp>
2023-10-30 21:20:23 +0900

    Viewからデータを受け取れるよう修正

--- src/main/java/com/example/todo/controller/task/TaskController.java
+++ src/main/java/com/example/todo/controller/task/TaskController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -35,5 +36,10 @@ public class TaskController {
     @GetMapping("/tasks/creationForm")
     public String showCreationForm() {
         return "tasks/form";
+    }
+
+    @PostMapping("/tasks")
+    public String create(TaskForm form, Model model) {
+        return list(model);
     }
 }
